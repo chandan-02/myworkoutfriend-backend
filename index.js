0f948a0f692cc5711bd3343597d83fbcb6f9adb8
chandan-02 <45965174+chandan-02@users.noreply.github.com>
2023-04-06 15:59:25 +0530

    done cors

--- index.js
+++ index.js
@@ -21,18 +21,18 @@ const Routes = require('./routes/index.js');
 const app = express();
 
 // enable cors
-var whitelist = ['*', 'http://localhost:3000']
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }, credentials: true, allowedHeaders: "Origin,X-Requested-With,Content-Type,Accept,Authorization"
-}
+// var whitelist = ['*', 'http://localhost:3000']
+// var corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }, credentials: true, allowedHeaders: "Origin,X-Requested-With,Content-Type,Accept,Authorization"
+// }
 app.use(cors());
-app.options('*', cors());
+// app.options('*', cors());
 
 
 //  Logging Middleware 
